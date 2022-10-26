541f00dda9a7d5c525fa5448939fcfbc9a80b352
amaan.ansari <amaan.ansari@vfirst.com>
2022-10-26 14:57:54 +0530

    drf demo

--- drf/apps/apis/models.py
+++ drf/apps/apis/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class GeeksModel(models.Model):
+    objects = None
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
